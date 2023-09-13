105c72ee1a953d92e69f8c6c309bb2507b152196
rodrigo <rodrigoxc97@hotmail.com>
2023-09-13 09:52:17 -0300

    feat: Ignite Gym

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import { Text, View, StatusBar } from "react-native";
+import { View, StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
+import { THEME } from "./src/theme";
+import { SignIn } from "@screens/Signin";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,27 +16,14 @@ export default function App() {
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
 
-      {fontsLoaded ? (
-        <Text style={{ fontFamily: "Roboto_700Bold", color: "#fff" }}>
-          Hello Word
-        </Text>
-      ) : (
-        <View />
-      )}
-    </View>
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
