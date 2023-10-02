9be80801d489785437f2f41e448737d3bac602a4
rodrigo <rodrigoxc97@hotmail.com>
2023-10-02 16:44:34 -0300

    feat: group

---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,30 @@
+import { UserPhoto } from "@components/UserPhoto";
+import { MaterialIcons } from "@expo/vector-icons";
+import { HStack, Heading, Icon, Text, VStack } from "native-base";
+
+export function HomeHeader() {
+  return (
+    <HStack bgColor={"gray.600"} pt={16} pb={5} px={8} alignItems={"center"}>
+      <UserPhoto
+        size={16}
+        alt="Imagem de perfil do usuário"
+        mr={4}
+        source={{
+          uri: "https://github.com/rodrigueira97.png",
+        }}
+      />
+
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize={"md"}>
+          Olá,
+        </Text>
+
+        <Heading color="gray.100" fontSize={"md"}>
+          Rodrigo
+        </Heading>
+      </VStack>
+
+      <Icon as={MaterialIcons} name="logout" color={"gray.200"} size={7} />
+    </HStack>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,33 @@
-import { Center, Text } from "native-base";
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { FlatList, HStack, VStack } from "native-base";
+import { useState } from "react";
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    "Costas",
+    "Bíceps",
+    "Triceps",
+    " Ombro",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("Costas");
+
   return (
-    <Center flex={1}>
-      <Text color="white">Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        horizontal
+        keyExtractor={(item) => item}
+        renderItem={(group) => (
+          <Group
+            name={group.item}
+            isActive={groupSelected === group.item}
+            onPress={() => setGroupSelected(group.item)}
+          />
+        )}
+      />
+    </VStack>
   );
 }
