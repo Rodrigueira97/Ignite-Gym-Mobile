62b04144383ae91173eb0f3b2276911f4c86bfa7
rodrigo <rodrigoxc97@hotmail.com>
2023-10-06 12:43:11 -0300

    fix: History anda profile

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,14 +1,49 @@
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
 
-import { VStack } from "native-base";
+import { Heading, SectionList, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "26.08.22",
+      data: ["Puxada frontal", "Remada unilateral"],
+    },
+    {
+      title: "27.08.22",
+      data: ["Puxada frontal"],
+    },
+  ]);
+
   return (
     <VStack>
       <ScreenHeader title={"Historico de exercicios"} />
 
-      <HistoryCard />
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color={"gray.200"} fontSize={"md"} mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={
+          exercises.length === 0 && {
+            height: 600,
+            justifyContent: "center",
+          }
+        }
+        ListEmptyComponent={() => (
+          <Text color={"gray.100"} textAlign={"center"}>
+            Não há exercícios registrados ainda.{"\n"} Vamos fazer exercícios
+            hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
     </VStack>
   );
 }
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,21 @@
-import { Center, Text } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { ScrollView, Text, VStack } from "native-base";
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <UserPhoto
+          source={{
+            uri: "https://github.com/rodrigueira97.png",
+          }}
+          alt="Foto de perfil"
+          size={33}
+        />
+      </ScrollView>
+    </VStack>
   );
 }
