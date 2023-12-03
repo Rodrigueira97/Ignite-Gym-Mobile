39f171e8b140dbc63dcd60ef8a6dba915132b5ac
Rodrigueira97 <rodrigoxc97@hotmail.com>
2023-12-03 13:25:28 -0300

    fix: context

--- Mobile/src/contexts/AuthContext.tsx
+++ Mobile/src/contexts/AuthContext.tsx
@@ -1,11 +1,13 @@
-import { ReactNode, createContext, useState } from 'react';
+import { ReactNode, createContext, useEffect, useState } from 'react';
 import { userDTO } from '@dtos/userDTO';
 import { api } from '@services/api';
+import { storageUserGet, storageUserSave } from '@storage/storageUser';
 
 interface PropsAuthContext {
   user: userDTO;
   setUser: React.Dispatch<React.SetStateAction<userDTO>>;
   signIn: (email: string, password: string) => Promise<void>;
+  isLoading: boolean;
 }
 
 interface PropsProvider {
@@ -16,6 +18,7 @@ export const AuthContext = createContext<PropsAuthContext>({} as PropsAuthContex
 
 export function AuthContextProvider({ children }: PropsProvider) {
   const [user, setUser] = useState<userDTO>({} as userDTO);
+  const [isLoading, setIsLoading] = useState(true);
 
   async function signIn(email: string, password: string) {
     // eslint-disable-next-line no-useless-catch
@@ -23,6 +26,7 @@ export function AuthContextProvider({ children }: PropsProvider) {
       const { data } = await api.post('/sessions', { email, password });
 
       if (data?.user) {
+        storageUserSave(data?.user);
         setUser(data?.user);
       }
     } catch (error) {
@@ -30,5 +34,25 @@ export function AuthContextProvider({ children }: PropsProvider) {
     }
   }
 
-  return <AuthContext.Provider value={{ user, setUser, signIn }}>{children}</AuthContext.Provider>;
+  async function loadUserData() {
+    try {
+      const userLogged = await storageUserGet();
+
+      if (userLogged) {
+        setUser(userLogged);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, signIn, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
