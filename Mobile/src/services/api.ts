b8f6ca693dcdcb2b26c19d6a6391d42c81e3acb5
Rodrigueira97 <rodrigoxc97@hotmail.com>
2023-12-21 23:13:20 -0300

    fix: services

--- Mobile/src/services/api.ts
+++ Mobile/src/services/api.ts
@@ -1,10 +1,19 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { AppError } from '@utils/AppError';
+
+interface SingOutProps {
+  signOut: () => void;
+}
+interface ApiInstaceProps extends AxiosInstance {
+  registerInterceptorTokenManager: ({ signOut }: SingOutProps) => () => void;
+}
 
 export const api = axios.create({
   timeout: 1000 * 30, // 30 seconds
   baseURL: 'http://127.0.0.1:3333',
-});
+}) as ApiInstaceProps;
+
+api.registerInterceptorTokenManager = (signOut) => {};
 
 api.interceptors.response.use(
   (response) => response,
